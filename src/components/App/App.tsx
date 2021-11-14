38b3010b1a46731614f2ca1e8a5fc68363397115
Anders Ramsay <819213+andersr@users.noreply.github.com>
2021-11-14 08:42:51 -0500

    Add bad console statement

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 export const APP_CONTAINER_TEST_ID = 'AppContainer'
 
 export function App () {
+  console.log('test with failure')
   return (
     <div data-testid={APP_CONTAINER_TEST_ID}>App component</div>
   )
