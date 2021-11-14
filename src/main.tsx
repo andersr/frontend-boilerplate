169d72baf0c59aa2fd4943e52936d781d872bc3e
Anders Ramsay <819213+andersr@users.noreply.github.com>
2021-11-14 08:12:06 -0500

    Enforce consistent test method

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,5 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import { App } from './components'
-// import { BrowserRouter } from 'react-router-dom';
-
-// import { App } from './components/App';
 
 ReactDOM.render(<App />, document.getElementById('root'))
