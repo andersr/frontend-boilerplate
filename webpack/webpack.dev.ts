da05ce5fa6bb0a34e217595818bcc311c461bee5
Anders Ramsay <819213+andersr@users.noreply.github.com>
2021-11-14 16:21:53 -0500

    Open browser for dev

--- webpack/webpack.dev.ts
+++ webpack/webpack.dev.ts
@@ -1,21 +1,22 @@
 import { merge } from 'webpack-merge'
-import { DevConfig } from './webpack';
-import { common } from './webpack.common';
+import { DevConfig } from './webpack'
+import { common } from './webpack.common'
 
-const API_HOST = 'http://localhost:8000';
+const API_HOST = 'http://localhost:8000'
 
 const config = merge(common as DevConfig, {
-  mode: 'development',
-  output: {
-    publicPath: '/'
-  },
-  devServer: {
-    port: 3000,
-    historyApiFallback: true,
-    proxy: {
-      '/api': API_HOST
+    mode: 'development',
+    output: {
+        publicPath: '/'
+    },
+    devServer: {
+        port: 3000,
+        historyApiFallback: true,
+        proxy: {
+            '/api': API_HOST
+        },
+        open: true
     }
-  }
-});
+})
 
-export default config;
+export default config
