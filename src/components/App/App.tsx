84c248edf55bd1d9322038121d496d228ba44d91
Anders Ramsay <819213+andersr@users.noreply.github.com>
2021-11-14 08:43:43 -0500

    Run linting first

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,7 +3,6 @@ import React from 'react'
 export const APP_CONTAINER_TEST_ID = 'AppContainer'
 
 export function App () {
-  console.log('test with failure')
   return (
     <div data-testid={APP_CONTAINER_TEST_ID}>App component</div>
   )
