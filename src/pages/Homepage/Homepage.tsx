e4bf0226be3f5ad1ac6c9d70124ae6a96c0d1c09
Anders Ramsay <819213+andersr@users.noreply.github.com>
2021-11-15 11:24:38 -0500

    Linting updates

--- src/pages/Homepage/Homepage.tsx
+++ src/pages/Homepage/Homepage.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { CurrentCount } from '../../components'
 import { SamplePngFile } from '../../assets/images'
 export const HOMEPAGE_TEST_ID = 'homepage'
 
 export function Homepage () {
+    const [foo, setFoo] = useState('')
+    useEffect(() => {
+        if (foo === 'dasdsd') {
+            console.warn('nad foo')
+        }
+    }, [foo])
+
     return (
         <div data-testid={HOMEPAGE_TEST_ID}><h1>Homepage</h1>
             <div>
@@ -12,6 +19,7 @@ export function Homepage () {
             </div>
             <CurrentCount />
             <img src={SamplePngFile} alt="Sample Image" />
+            <button onClick={() => setFoo('bla')}>Set FOO</button>
         </div>
     )
 }
