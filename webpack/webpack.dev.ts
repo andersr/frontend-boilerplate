534c0437abbfbbaf458bee45d7759f8f272e7d62
Anders Ramsay <819213+andersr@users.noreply.github.com>
2021-11-15 15:15:45 -0500

    Add smart open for dev

--- webpack/webpack.dev.ts
+++ webpack/webpack.dev.ts
@@ -1,21 +1,27 @@
+import openBrowser from 'react-dev-utils/openBrowser'
 import { merge } from 'webpack-merge'
+
 import { DevConfig } from './webpack'
 import { common } from './webpack.common'
 
 const API_HOST = 'http://localhost:8000'
+const PORT = 3000
 
 const config = merge(common as DevConfig, {
-    mode: 'development',
-    output: {
-        publicPath: '/'
+  mode: 'development',
+  output: {
+    publicPath: '/'
+  },
+  devServer: {
+    port: PORT,
+    historyApiFallback: true,
+    proxy: {
+      '/api': API_HOST
     },
-    devServer: {
-        port: 3000,
-        historyApiFallback: true,
-        proxy: {
-            '/api': API_HOST
-        }
+    onListening: () => {
+      openBrowser(`http://localhost:${PORT}`)
     }
+  }
 })
 
 export default config
