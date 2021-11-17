87296028c1149c3da13c019c5d0429e76ac7b517
Anders Ramsay <819213+andersr@users.noreply.github.com>
2021-11-17 07:44:06 -0500

    Add Fullheight (#1)
    * Add jest snapshots
    
    * Add full height layout

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,14 +1,31 @@
+import '../../styles/global.css'
+
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import styled from 'styled-components'
 
 export const APP_CONTAINER_TEST_ID = 'AppContainer'
 
+const Container = styled.div.attrs(() => ({
+  'data-testid': APP_CONTAINER_TEST_ID
+}))`
+  min-height: 100%;
+  display: flex;
+  flex-direction: column;
+`
+
+const MainContent = styled.div`
+  flex: 1;
+`
+
 export function App() {
   return (
-    <div data-testid={APP_CONTAINER_TEST_ID}>
+    <Container>
       <div>APP HEADER</div>
-      <Outlet />
+      <MainContent>
+        <Outlet />
+      </MainContent>
       <div>APP FOOTER</div>
-    </div>
+    </Container>
   )
 }
