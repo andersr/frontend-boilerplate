874279febf6f3775c936dc7514ce2c4d2ca3a6a3
Anders Ramsay <819213+andersr@users.noreply.github.com>
2021-11-19 20:24:22 -0500

    Rm button test

--- src/pages/Homepage/Homepage.tsx
+++ src/pages/Homepage/Homepage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
 
 import { SamplePngFile } from '../../assets/images'
@@ -6,13 +6,6 @@ import { CurrentCount } from '../../components'
 export const HOMEPAGE_TEST_ID = 'homepage'
 
 export function Homepage() {
-  const [foo, setFoo] = useState('')
-  useEffect(() => {
-    if (foo === 'dasdsd') {
-      console.warn('nad foo')
-    }
-  }, [foo])
-
   return (
     <div data-testid={HOMEPAGE_TEST_ID}>
       <h1>Homepage</h1>
@@ -21,7 +14,6 @@ export function Homepage() {
       </div>
       <CurrentCount />
       <img src={SamplePngFile} alt="Sample Image" />
-      <button onClick={() => setFoo('bla')}>Set FOO</button>
     </div>
   )
 }
